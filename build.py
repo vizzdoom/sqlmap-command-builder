5b6c543e198c464ab1bab13f18bd0d3aad86b22b
Adrian `Vizz` Michalczyk <vizzdoom@gmail.com>
2025-06-16 19:29:39 +0200

    Automation script to build the project

---
+++ build.py
@@ -0,0 +1,20 @@
+# build.py
+html = open('index.html').read()
+css = open('style.css').read()
+js = open('app.js').read()
+
+# Zakładam, że index.html ma <head>...</head> i </body>
+# Dodaj CSS do <head>
+html = html.replace(
+    '</head>',
+    f'<style>\n{css}\n</style>\n</head>'
+)
+# Dodaj JS na końcu przed </body>
+html = html.replace('<script src="app.js"></script>','');
+html = html.replace(
+    '</body>',
+    f'<script>\n{js}\n</script>\n</body>'
+)
+
+with open('index-final.html', 'w') as f:
+    f.write(html)
