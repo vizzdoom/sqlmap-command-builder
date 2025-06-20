7e6a39ea2ae806ec52109c0b6576e212cd4865e2
Adrian `Vizz` Michalczyk <vizzdoom@gmail.com>
2025-06-21 01:10:59 +0200

    Bug fix

--- build.py
+++ build.py
@@ -7,11 +7,11 @@ def read_text(path):
 
 def inline_assets(html, css, js):
     compilation_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    html = html.replace(f'<span id="tool-version"></span>', '<span id="tool-version">compiled on {compilation_time}<strong></strong></span>')
-    html = html.replace('<link rel="stylesheet" href="style.css">', '')
-    html = html.replace('</head>', f'<style>\n{css}\n</style>\n</head>')
-    html = html.replace('<script src="app.js"></script>', '')
-    html = html.replace('</body>', f'<script>\n{js}\n</script>\n</body>')
+    html = html.replace(f'<span id="tool-version"></span>', f'<span id="tool-version">compiled on {compilation_time}<strong></strong></span>')
+    html = html.replace(f'<link rel="stylesheet" href="style.css">', f'')
+    html = html.replace(f'</head>', f'<style>\n{css}\n</style>\n</head>')
+    html = html.replace(f'<script src="app.js"></script>', f'')
+    html = html.replace(f'</body>', f'<script>\n{js}\n</script>\n</body>')
     return html
 
 
