35fe1ef9eb436b4477ed645872f5ad5993c5214e
Adrian `Vizzdoom` Michalczyk <vizzdoom@gmail.com>
2025-06-25 16:25:02 +0200

    Fixed text align in buttons

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def compile_assets(html, css, js):
     compilation_time = datetime.datetime.now().astimezone(pytz.timezone('Europe/Warsaw')).strftime("%Y-%m-%d %H:%M")
     img_background = f'background: url("data:image/jpeg;base64,{base64.b64encode(open("background.jpg", "rb").read()).decode()}");'
     css = css.replace(f"  background: url('background.jpg');", img_background)
-    html = html.replace(f'<span id="tool-version"></span>', f'<span id="tool-version">compiled on <strong>{compilation_time}</strong></span>')
+    html = html.replace(f'<span id="tool-version"></span>', f'<span id="tool-version">compilation <strong>{compilation_time}</strong></span>')
     html = html.replace(f'<link rel="stylesheet" href="style.css">', f'')
     html = html.replace(f'</head>', f'<style>\n{css}\n</style>\n</head>')
     html = html.replace(f'<script src="app.js"></script>', f'')
