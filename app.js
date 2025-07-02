c60994cd768f4c50c0b36fcc98b49721a0c2db3c
Adrian `Vizz` Michalczyk <vizzdoom@gmail.com>
2025-07-02 19:18:30 +0200

    Beta release - Code refactoring - New background - Better colors - UX improvements - Better tooltips

--- app.js
+++ app.js
@@ -531,7 +531,7 @@ class SQLMapGenerator {
             switch (levelSlider.value) {
                 case "1": levelHelp = "1: Fastest and least intrusive testing of GET and POST parameters (default)."; break;
                 case "2": levelHelp = "2: Additionally test injections in the Cookie header."; break;
-                case "3": levelHelp = "3: Additionally test injections in User-Agent and Referer headers ."; break;
+                case "3": levelHelp = "3: Additionally test injections in User-Agent and Referer headers."; break;
                 case "4": levelHelp = "4: Additionally perform more advanced tests, such as null values and some extra payloads."; break;
                 case "5": levelHelp = "5: Additionally test Host header, using all possible payloads."; break;
             }
